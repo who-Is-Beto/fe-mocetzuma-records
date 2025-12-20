6c3ee64212c7a3624eb82e78c644d53510620153
whoIsBeto <whoisbeto@gmail.com>
2025-12-20 03:30:29 -0600

    [fix] env name import

--- src/app/config/api.ts
+++ src/app/config/api.ts
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
   typeof import.meta.env !== "undefined" && import.meta.env.VITE_API_URL
-    ? String(import.meta.env.API_URL)
+    ? String(import.meta.env.VITE_API_URL)
     : "http://localhost:8008";
